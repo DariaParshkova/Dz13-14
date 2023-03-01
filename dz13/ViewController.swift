a15f013a05d611c064cd46e49e4699e0077a75f3
DariaParshkova <neifalen@ynadex.ru>
2023-03-01 14:09:24 +0300

    Initial Commit

---
+++ dz13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dz13
+//
+//  Created by Parshkova Daria on 01.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
